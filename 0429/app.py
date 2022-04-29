0f9fdcbb96f60705ef54dd1d8a265bd36cae7dfd
kwn1996kr <kwn1996kr@naver.com>
2022-04-29 20:46:05 +0900

    "220429"

---
+++ 0429/app.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.test
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detail/<idx>')
+def detail(idx):
+    // todo
+
+@app.route('/post', methods=['POST'])
+def save_post():
+    // todo
+    return {"result": "success"}
+
+
+@app.route('/post', methods=['GET'])
+def get_post():
+    posts = 0
+		// todo
+    return jsonify({"posts": posts})
+
+
+@app.route('/post', methods=['DELETE'])
+def delete_post():
+    idx = request.args.get('idx')
+    db.test.delete_one({'idx': int(idx)})
+    return {"result": "success"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
